c1d6ae369e2456e553ba6dfa479447f39a24a9c4
Ismael Treviño <ismolahiena@gmail.com>
2025-03-03 11:30:35 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import netlify from "@astrojs/netlify";
+import node from '@astrojs/node';
+
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
@@ -30,7 +32,7 @@ export default defineConfig({
     remarkPlugins: [remarkReadingTime],
   },
   output: "server",
-  adapter: netlify({ edgeMiddleware: true }),
+  adapter: node({ mode: 'standalone' }), // O usa 'middleware' si prefieres
   vite: {
     assetsInclude: "**/*.riv",
   },
