b3e3401aea9375a698b2369038b6a22d5bd2cfa0
Ismael Treviño <ismolahiena@gmail.com>
2025-06-10 22:16:38 +0200

    Agrega configuración de sitio y base en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ismola.dev",
+  base: "/",
   vite: {
     preview: {
       // Permite cualquier host en preview
